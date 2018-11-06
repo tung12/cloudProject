f98aaf0a6f6d9c39f8206fe01d4340df6fafe5be
tung12 <ntientung01@gmail.com>
2018-11-06 09:11:35 +0700

    www

--- src/main/webapp/src/service/apiService.js
+++ src/main/webapp/src/service/apiService.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 
 export let MAIN_API = Axios.create({
     //BE local
-    baseURL: ''
+    baseURL: '/api'
     //BE production
      //baseURL: 'https://cloudproject123.herokuapp.com/api'
 });
